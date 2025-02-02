caf04433f9e14cfa5acda79cfa6da63c7cd96a50
Herdeson Mourao <herdesondev@gmail.com>
2025-02-01 21:02:34 -0300

    feat: basic api creation command

---
+++ src/commands/new.ts
@@ -0,0 +1,57 @@
+import { GluegunCommand, GluegunToolbox } from "gluegun";
+
+const command: GluegunCommand = {
+  name: 'new',
+  alias: ['n'],
+  description: 'Create new Node API Project',
+  run: async (toolbox: GluegunToolbox) => {
+    const { 
+      parameters,
+      filesystem, 
+      template: { generate }, 
+      print, 
+      packageManager } = toolbox;
+
+    const projectName = parameters.first;
+
+    if (!projectName) {
+      print.error('Please provide a project name!');
+
+      process.exit(1);
+    }
+
+    const dirs = [
+      'src/controllers',
+      'src/services',
+      'src/repositories',
+      'src/models',
+      'src/dtos',
+      'src/routes',
+    ]
+
+    dirs.forEach((dir: string) => {
+      filesystem.dirAsync(`${projectName}/${dir}`);
+    })
+
+    await generate({
+      template: 'base/gitignore.ejs',
+      target: `${projectName}/.gitignore`,
+    })
+
+    await generate({
+      template: 'base/package.json.ejs',
+      target: `${projectName}/package.json`,
+      props: { projectName }
+    });
+
+    // Install devDependencies
+    await packageManager.add([
+      'typescript',
+      '@types/node',
+      'tsup',
+      'tsx',
+    ], { dev: true, dryRun: false });
+  }
+}
+
+export default command;
